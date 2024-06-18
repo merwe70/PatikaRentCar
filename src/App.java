2de0242cd7ad1a5c2d0215137f567ea83d2be80b
merwe70 <58660978+merwe70@users.noreply.github.com>
2024-06-19 00:19:43 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import Core.Helper;
+import View.LoginView;
+
+public class App  {
+
+    public static void main(String[] args) {
+        Helper.setTheme();
+        LoginView loginview = new LoginView();
+    }
+}
---
+++ src/View/LoginView.java
@@ -0,0 +1,43 @@
+package View;
+
+import Business.UserManager;
+import Core.Helper;
+import Entity.User;
+
+import javax.swing.*;
+
+
+public class LoginView extends Layout {
+    private JPanel container;
+    private JPanel w_top;
+    private JLabel lbl_welcome;
+    private JLabel lbl_welcome2;
+    private JPanel w_bottom;
+    private JTextField fld_username;
+    private JPasswordField fld_password;
+    private JButton btn_login;
+    private JLabel lbl_username;
+    private JLabel lbl_password;
+    private final UserManager userManager;
+
+    public LoginView() {
+        userManager = new UserManager();
+        this.add(container);
+        this.guiInitialize(300, 300);
+        btn_login.addActionListener(e -> {
+            JTextField[] checkFieldList = {this.fld_username, this.fld_password};
+            if (Helper.isFieldListEmpty(checkFieldList)) {
+                Helper.showMessage("fill");
+            } else {
+                User loginUser = this.userManager.findByLogin(this.fld_username.getText(), this.fld_password.getText());
+                if (loginUser == null) {
+                    Helper.showMessage("notFound");
+                } else {
+                    AdminView adminView = new AdminView(loginUser);
+                    dispose();
+                }
+            }
+
+        });
+    }
+}
